0e04bd5a8dfc82cf106bcacdfe3aac86d48a4f1b
Oleksandr Deundiak <deundiak@gmail.com>
2019-09-06 16:19:07 +0300

    Adding tests

---
+++ purekit/src/test/java/com/virgilsecurity/purekit/protocol/Testt.java
@@ -0,0 +1,14 @@
+package com.virgilsecurity.purekit.protocol;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class Testt {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
