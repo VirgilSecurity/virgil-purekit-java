946696ade595d2fc7874e110be7e9b7fb9277445
Andrii Iakovenko <iakovenko.andrii@gmail.com>
2020-03-04 10:30:47 +0200

    Clean gh-pages branch

--- purekit/src/main/java/com/virgilsecurity/purekit/storage/mariadb/MariaDbOperationNotSupportedException.java
+++
@@ -1,9 +0,0 @@
-package com.virgilsecurity.purekit.storage.mariadb;
-
-import com.virgilsecurity.purekit.storage.exception.PureStorageException;
-
-/**
- * MariaDb storage operation not supported exception
- */
-public class MariaDbOperationNotSupportedException extends PureStorageException {
-}
--- purekit/src/test/resources/com/virgilsecurity/purekit/setuptestdb.sh
+++
@@ -1,5 +0,0 @@
-docker stop puredb
-docker rm puredb
-CONT=`docker run --name puredb -e MYSQL_ROOT_PASSWORD=qwerty -p 3306:3306 -d mariadb:latest`
-sleep 10
-cat setupdb.sql | docker exec -i $CONT /usr/bin/mysql ""
